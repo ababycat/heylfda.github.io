06ac40f7b61095c99da7f78ed4575fe4728099e4
MaXinglong <mxl12138@hrbeu.edu.cn>
2019-06-17 11:41:43 +0800

    arrow

--- arrow/script/skecth.js
+++ arrow/script/skecth.js
@@ -1,73 +1,101 @@
 "using strict";
-const MIN_LIVE_LEN = 120.0;
+const MIN_LEN_RATE = 0.8;
+const MAX_LEN_RATE = 1;
+const MIN_LIVE_LEN_RATE = 0.6;
+const LEN_DECAY = 0.99;
+const CENTER_DIST_DECAY = 0.95;
+const MAX_RADIUS_RATE = 0.8;
+const MIN_RADIUS_RATE = 0.5;
+const MAX_NUM = 300;
 
-const noiseScale = 0.02;
-let x=0, y=0;
+const RANDOM_P = 0.8;
 
+let nx = 0, ny = 0;
 let idx = 1000;
 let idy = 0;
 
+let START_COLOR;
+let END_COLOR;
 
-let MovingArrow = function(arrow_len, center_dist, angle, cx, cy, radius){
-    this.arrow_len = arrow_len;
-    this.center_dist = center_dist;
-    this.angle = angle;
-    this.cx=cx;
-    this.cy=cy;
-    this.radius=radius;
-
-    console.log(this.arrow_len);
-    this.update = function(){
-
-    }
-
-    this.display = function(){
-        push();
-        fill(255, 0, 255);
-        ellipse(10, 10, 50, 50);
-        pop();
-    }
-
-    this.len = function(){
-        return this.arrow_len;
-    }
-
-    this.alive = () => !(this.len() < MIN_LIVE_LEN);
-}
-
-let p;
+let plist = [];
 
 function setup() {
-    let myCanvas = createCanvas(300, 300);
+    START_COLOR = color("#6495ED");
+    END_COLOR = color("#FF8C00");
+
+    let myCanvas = createCanvas(500, 500);
     myCanvas.parent('myContainer');
-    p = new MovingArrow(1, 2, 3, 4, 5, 6);
+    // p = new MovingArrow(150, 10, PI/4, width, height, 150);
 }
 
 function draw() {
-    
-    
-    background(0);
-  
-    x = noise(idx) * width;
-    y = noise(idy) * height; 
-    
-    idx += 0.01;
-    idy += 0.01;
-    
-    circle(x, y, 55);
-    ellipse(mouseX, mouseY, 50, 50);
+    let radius = Math.sqrt(Math.pow(width/2, 2) + Math.pow(height/2, 2));
+    let toPixel = (x) => x * radius;
 
-    // ++p;
-    // console.log(p);
-    p.display();
+    background(255);
+    
+
+    nx = noise(idx) * width;
+    ny = noise(idy) * height;
+
+    idx += 0.001;
+    idy += 0.001;
+
+    plist.forEach((x) => x.update(nx, ny));
+
+    plist = plist.filter((x) => x.alive());
+
+    plist.forEach((x) => x.display());
+
+    if((plist.length < MAX_NUM) && (random() < RANDOM_P)){
+        plist.push(new MovingArrow(
+            random(toPixel(MIN_LEN_RATE), toPixel(MAX_LEN_RATE)),
+            random(toPixel(MIN_RADIUS_RATE), toPixel(MAX_RADIUS_RATE)),
+            random(0, TWO_PI),
+            nx, ny,
+            radius
+        ));
+    }
 }
 
 
-// function mousePressed() {
-//   if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-//     let fs = fullscreen();
-//     fullscreen(!fs);
-//   }
-// }
+let MovingArrow = function (arrow_len, center_dist, angle, cx, cy, radius) {
+    this.arrow_len = arrow_len;
+    this.center_dist = center_dist;
+    this.angle = angle;
+    this.cx = cx;
+    this.cy = cy;
+    this.radius = radius;
+    this.arrow_color = lerpColor(START_COLOR, END_COLOR, this.center_dist / this.radius);
+
+    this.update = function (cx, cy) {
+        this.cx = cx;
+        this.cy = cy;
+        this.arrow_len *= LEN_DECAY;
+        this.center_dist *= CENTER_DIST_DECAY;
+        this.arrow_color = lerpColor(START_COLOR, END_COLOR, this.center_dist / this.radius);
+    }
+
+    this.display = function () {
+        push();
+        translate(this.cx, this.cy);
+        rotate(this.angle);
+        strokeWeight(2);
+        stroke(this.arrow_color);
+        line(0, this.center_dist, 0, this.center_dist + this.arrow_len);
+        pop();
+    }
+
+    this.len = () => this.arrow_len;
+
+    this.alive = () => !(this.len() < MIN_LIVE_LEN_RATE * this.radius);
+}
 
 
+function mousePressed() {
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    let fs = fullscreen();
+    fullscreen(!fs);
+    // fullscreen();
+  }
+}
